06410f6dbc1abe3ec6b03da6b137c61a3ad18dbc
David Lyons <david.scott.lyons@gmail.com>
2025-10-17 14:50:34 -0700

    album image fallback icon

--- src/components/AlbumGrid.tsx
+++ src/components/AlbumGrid.tsx
@@ -1,23 +1,26 @@
 import type { ReleaseJSONType } from '@/lib/types'
+import { Disc3 } from 'lucide-react'
 
 export const AlbumGrid = ({ releases }: { releases: ReleaseJSONType[] }) => (
   <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5">
     {releases.map(({ url, title, artist, year, format, thumb }, index) => (
       <a
         href={url}
-        className="flex gap-4 p-3 hover:bg-foreground/8 rounded-sm overflow-hidden"
+        className="hover:bg-foreground/8 flex gap-4 overflow-hidden rounded-sm p-3"
         key={`${title}-${index}`}
       >
-        <img
-          src={thumb || 'https://placehold.co/96?text=404'}
-          alt={title}
-          className="size-24 object-cover"
-        />
+        {thumb ? (
+          <img src={thumb} alt={title} className="size-24 object-cover" />
+        ) : (
+          <div className="bg-card flex size-24 items-center justify-center">
+            <Disc3 size={64} className="opacity-15" />
+          </div>
+        )}
         <div className="grow">
-          <h2 className="font-bold truncate max-w-40">{title}</h2>
-          <p className="mb-2 truncate max-w-40">{artist}</p>
+          <h2 className="max-w-40 truncate font-bold">{title}</h2>
+          <p className="mb-2 max-w-40 truncate">{artist}</p>
           <p className="text-sm">{year}</p>
-          <p className="text-sm opacity-50 truncate max-w-40">{format}</p>
+          <p className="max-w-40 truncate text-sm opacity-50">{format}</p>
         </div>
       </a>
     ))}
