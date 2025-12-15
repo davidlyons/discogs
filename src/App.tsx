1affd7b8667119b153efd877882ecc694f0b5a49
David Lyons <david.scott.lyons@gmail.com>
2025-12-14 17:44:03 -0800

    added github repo link

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { ExternalLink } from 'lucide-react'
 import { VinylBrowser } from '@/components/VinylBrowser'
+import { GitHubLogoIcon } from '@radix-ui/react-icons'
 
 export const App = () => {
   const [user, setUser] = useState('davidlyons')
   const [page, setPage] = useState(1)
 
   return (
-    <div className="overflow-hidden py-14">
+    <div className="overflow-hidden pt-14 pb-8 min-[1680px]:pb-14">
       <div className="container">
         <div className="mb-5 flex items-center gap-2 text-sm">
           <span className="font-bold">Discogs user</span>
@@ -35,6 +36,18 @@ export const App = () => {
         </div>
 
         <VinylBrowser user={user} page={page} setPage={setPage} />
+      </div>
+
+      <div className="bottom-4 left-4 text-center max-[1680px]:mt-8 min-[1680px]:fixed">
+        <a
+          href="https://github.com/davidlyons/discogs"
+          aria-label="GitHub Repo"
+          className="inline-block p-4 opacity-50 transition-opacity hover:opacity-100"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GitHubLogoIcon className="size-6" />
+        </a>
       </div>
     </div>
   )
